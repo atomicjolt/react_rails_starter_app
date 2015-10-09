d183364fafbe32a12f88010b13ee5209670f4c6a
Justin Ball <justinball@gmail.com>
2015-10-09 16:11:28 -0600

    Added additional helper methods for user and course id from LTI request

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,16 @@ module ApplicationHelper
 
   def jwt_token
     return unless signed_in?
-    AuthToken.issue_token({ user_id: current_user.id })
+    AuthToken.issue_token({ user_id: current_user.id, canvas_roles: params["roles"] })
   end
+
+  def course_id
+    return params[:custom_canvas_course_id]
+  end
+
+  def user_id
+    return params[:custom_canvas_user_id]
+   end
 
   def client_images(*images)
     map = images.map { |image| %Q{#{image.gsub('/', '_').gsub('.', '_')} : "#{image_path(image)}"} }
