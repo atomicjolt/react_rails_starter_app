9d33f6ab0eba96dfd23f2e10f55802992842608c
Justin Ball <justinball@gmail.com>
2015-10-09 14:07:25 -0600

    Include babel runtime

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -13,8 +13,7 @@ module.exports = function(release){
   ];
 
   var autoprefix = '{browsers:["Android 2.3", "Android >= 4", "Chrome >= 20", "Firefox >= 24", "Explorer >= 8", "iOS >= 6", "Opera >= 12", "Safari >= 6"]}';
-  var jsLoaders = ["babel-loader?stage=0"];
-  //var jsLoaders = ["babel-loader?stage=0&optional=runtime"]; // include the runtime 
+  var jsLoaders = ["babel-loader?stage=0&optional=runtime"]; // include the runtime 
 
   var cssLoaders = ['style-loader', 'css-loader', 'autoprefixer-loader?' + autoprefix];
 
