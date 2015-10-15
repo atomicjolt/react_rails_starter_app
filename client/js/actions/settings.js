ea69ba54bb9c2cdcddaf28cacf1a0394ffc6973b
Justin Ball <justinball@gmail.com>
2015-10-15 16:37:00 -0600

    Added code to refresh jwt

--- client/js/actions/settings.js
+++ client/js/actions/settings.js
@@ -2,11 +2,19 @@
 
 import Constants   from   "../constants";
 import Dispatcher  from   "../dispatcher";
+import Api         from   "./api";
 
 export default {
 
   load(defaultSettings){
     Dispatcher.dispatch({ action: Constants.SETTINGS_LOAD, data: defaultSettings });
+    if(defaultSettings.userId){
+      // Add timeout to refresh jwt token
+      var refresh = 1000 * 60 * 20; // every 20 minutes
+      setInterval(function(){ 
+        Api.get(Constants.REFRESH_JWT, `api/sessions/${defaultSettings.userId}`);
+      }, refresh);
+    }
   }
 
 };
