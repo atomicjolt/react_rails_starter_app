e298b1adee8eaf55d0a3aca9f578fdc381c0ce1b
Justin Ball <justinball@gmail.com>
2015-10-15 15:38:38 -0600

    Renamed json value

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
   def show
     token = AuthToken.issue_token({ user_id: current_user.id })
     respond_to do |format|
-      format.json { render json: { jwt_token: token } }
+      format.json { render json: { jwt: token } }
     end
   end
 
