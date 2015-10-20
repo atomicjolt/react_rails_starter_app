c8db4da03321f09d4aaaa2c78ab949fde21bec58
Joel Duffin <joel.duffin@gmail.com>
2015-10-20 12:33:27 -0600

    Updated database example and schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312162723) do
+ActiveRecord::Schema.define(version: 20150817203909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
