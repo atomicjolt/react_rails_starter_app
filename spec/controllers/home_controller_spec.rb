a6263ae1fed8218a2946f74d63f073474ec5becf
Justin Ball <justinball@gmail.com>
2015-10-21 11:46:35 -0600

    Added getting started instructions to home page.

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe HomeController, type: :controller do
+  
+  render_views
+
+  before do
+    @account = FactoryGirl.create(:account)
+    allow(controller).to receive(:current_account).and_return(@account)
+  end
+
+  describe "index" do
+
+    it "loads the home page" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    context "Signed In" do
+      
+      login_user
+
+      it "include LTI instructions" do
+        config = Lti::Canvas.basic_config(@account.code)
+        xml = Lti::Canvas.config_xml(config)
+        get :index
+        expect(response.body).to include(CGI.escapeHTML(xml))
+      end
+    end
+
+  end
+
+end
