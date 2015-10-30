a2e6dc367c52e3ef2ba585e3d8b588239199a890
Justin Ball <justinball@gmail.com>
2015-10-29 20:38:42 -0600

    Cleaned up routes

--- client/js/routes.jsx
+++ client/js/routes.jsx
@@ -1,11 +1,10 @@
 "use strict";
 
-import React        from 'react';
-import Route        from 'react-router';
-import IndexRoute   from 'react-router';
-import Index        from './components/index';
-import Home         from './components/main/home';
-import NotFound     from './components/not_found';
+import React                  from 'react';
+import { Route, IndexRoute }  from 'react-router';
+import Index                  from './components/index';
+import Home                   from './components/main/home';
+import NotFound               from './components/not_found';
 
 export default (
   <Route path="/" component={Index}>
