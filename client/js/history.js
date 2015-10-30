e5dc212d700035e855f3455bf62095a3d9bd1259
Justin Ball <justinball@gmail.com>
2015-10-30 13:51:12 -0600

    Removed client app

--- client/js/history.js
+++
@@ -1,5 +0,0 @@
-// import createBrowserHistory from 'history/lib/createBrowserHistory';
-// export default createBrowserHistory();
-
-import { createHashHistory } from 'history';
-export default new createHashHistory();
--- client/styles/styles.js
+++
@@ -1,3 +0,0 @@
-// This file exists to compile all css - less and scss - into one file
-require('./styles.less');
-require('./styles.scss');
