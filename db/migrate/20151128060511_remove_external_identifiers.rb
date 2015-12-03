6e75ef5c7ae42d53ee75b106c6f6a5128f71aa53
Joel Duffin <joel.duffin@gmail.com>
2015-12-03 11:19:40 -0700

    Fixed bad migration

--- db/migrate/20151128060511_remove_external_identifiers.rb
+++ db/migrate/20151128060511_remove_external_identifiers.rb
@@ -3,10 +3,6 @@ class RemoveExternalIdentifiers < ActiveRecord::Migration
     add_column :users, :lti_identifier, :string
     add_column :users, :lti_user_id, :string
     add_column :users, :lti_sis_user_id, :string
-    User.all.each do |user|
-      ei = user.external_identifiers.first      
-      user.update_attributes({lti_identifier: ei.identifier, lti_user_id: ei.external_user_id}) if ei
-    end
     drop_table :external_identifiers
     add_index :users, :lti_identifier
     add_index :users, :lti_user_id
