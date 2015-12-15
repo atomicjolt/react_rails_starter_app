615651298a0c3f36decf639ce6bd08f2ab0664ce
Joel Duffin <joel.duffin@gmail.com>
2015-12-15 13:31:33 -0700

    Courses factory needs to give new courses an instructor

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -3,7 +3,10 @@ FactoryGirl.define do
   factory :course do
     name { FactoryGirl.generate(:name) }
     account
-    after(:build) { |course| course.users << FactoryGirl.create(:user, account_id: course.account_id) }
+    after(:create) do |course| 
+      instructor = FactoryGirl.create(:user, account_id: course.account_id)
+      UserCourse.create!(course_id: course.id, user_id: instructor.id, role_id: UserCourse::INSTRUCTOR)
+    end
   end
 
 end
