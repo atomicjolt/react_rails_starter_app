892d36e646dc842bb6f4852916bf869186658f38
Justin Ball <justinball@gmail.com>
2015-12-15 18:11:13 -0700

    Added message specs

--- client/js/reducers/messages.js
+++ client/js/reducers/messages.js
@@ -3,7 +3,6 @@
 import Immutable   from "immutable";
 import ActionTypes from "../constants/action_types";
 import ErrorTypes  from "../constants/error";
-import _           from "lodash";
 
 const initialState = Immutable.fromJS([]);
 
@@ -12,28 +11,27 @@ export default (state = initialState, action) => {
   switch(action.type){
 
     case ErrorTypes.TIMEOUT:
-      return state.push(`Request timed out. Response was: ${action.payload}`);
+      return state.push(`Request timed out. Response was: ${action.payload.message}`);
       break;
 
     case ErrorTypes.NOT_AUTHORIZED:
-      return state.push(`Request not authorized: ${action.payload}`);
+      return state.push(`Request not authorized: ${action.payload.message}`);
       break;
 
     case ErrorTypes.ERROR:
-      return state.push(`Request error: ${action.payload}`);
+      return state.push(`Request error: ${action.payload.message}`);
       break;
 
     case ActionTypes.ADD_MESSAGE:
-      return state.push(action.payload);
+      return state.push(action.payload.message);
       break;
 
     case ActionTypes.REMOVE_MESSAGE:
-      return state.push(action.payload);
-      removeMessage(payload.messageId);
+      return state.filter(message => message != action.payload.message);
       break;
 
     case ActionTypes.CLEAR_MESSAGES:
-      clearMessages();
+      return initialState;
       break;
 
     default:
--- client/js/reducers/messages.spec.js
+++ client/js/reducers/messages.spec.js
@@ -1,4 +1,10 @@
-import messages        from "./messages";
+"use strict";
+
+import _           from "lodash";
+import Immutable   from "immutable";
+import ActionTypes from "../constants/action_types";
+import ErrorTypes  from "../constants/error";
+import messages    from "./messages";
 
 describe("message reducer", () => {
 
@@ -10,5 +16,113 @@ describe("message reducer", () => {
     });
     
   });
+
+  describe("TIMEOUT", () => {
+
+    it("adds a timeout message", () => {
+      const initialState = Immutable.fromJS([]);
+      const state = messages(initialState, {
+        type: ErrorTypes.TIMEOUT,
+        payload: {
+          message: "No response"
+        }
+      });
+      expect(_.contains(state.get(0),"Request timed out")).toBe(true);
+    });
+
+  });
+
+  describe("NOT_AUTHORIZED", () => {
+
+    it("Adds a not authorized error", () => {
+      const initialState = Immutable.fromJS([]);
+      const state = messages(initialState, {
+        type: ErrorTypes.NOT_AUTHORIZED,
+        payload: {
+          message: "Not authorized"
+        }
+      });
+      expect(_.contains(state.get(0),"Request not authorized")).toBe(true);
+    });
+
+  });
+
+  describe("ERROR", () => {
+
+    it("Adds an error", () => {
+      const initialState = Immutable.fromJS([]);
+      const state = messages(initialState, {
+        type: ErrorTypes.ERROR,
+        payload: {
+          message: "Error"
+        }
+      });
+      expect(_.contains(state.get(0),"Request error")).toBe(true);
+    });
+
+  });
+
+  describe("ADD_MESSAGE", () => {
+
+    it("Adds a message", () => {
+      const initialState = Immutable.fromJS([]);
+      const action = {
+        type: ActionTypes.ADD_MESSAGE,
+        payload: {
+          message: "Things are great"
+        }
+      }
+      const state = messages(initialState, action);
+      expect(_.contains(state.get(0),action.payload.message)).toBe(true);
+    });
+
+  });
+
   
+  describe("REMOVE_MESSAGE", () => {
+
+    it("Removes message", () => {
+      const action = {
+        type: ActionTypes.ADD_MESSAGE,
+        payload: {
+          message: "Things are great"
+        }
+      };
+      const removeAction = {
+        type: ActionTypes.REMOVE_MESSAGE,
+        payload: {
+          message: "Things are great"
+        }
+      };
+      const initialState = Immutable.fromJS([action.payload.message]);
+      const state = messages(initialState, action);
+      expect(_.contains(state.get(0),action.payload.message)).toBe(true);
+      const newState = messages(state, removeAction);
+      expect(newState.toJS()).toEqual([]);
+    });
+
+  });
+
+  describe("CLEAR_MESSAGES", () => {
+
+    it("Removes message", () => {
+      const action = {
+        type: ActionTypes.ADD_MESSAGE,
+        payload: {
+          message: "Things are great"
+        }
+      };
+      const removeAction = {
+        type: ActionTypes.CLEAR_MESSAGES
+      };
+      const initialState = Immutable.fromJS([action.payload.message]);
+      const state = messages(initialState, action);
+      expect(_.contains(state.get(0),action.payload.message)).toBe(true);
+      const newState = messages(state, removeAction);
+      expect(newState.toJS()).toEqual([]);
+    });
+
+  });
+
+
 });
