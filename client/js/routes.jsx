f008bc5a0791e9c0a75a16335c8f89dd7f1b1203
Justin Ball <justinball@gmail.com>
2015-12-15 18:11:41 -0700

    Removes unused files.

--- client/js/routes.jsx
+++ client/js/routes.jsx
@@ -1,7 +1,6 @@
 "use strict";
 
 import React                  from 'react'; // if you use jsx, you have to have React imported
-import Router                 from 'react-router';
 import { Route, IndexRoute }  from 'react-router';
 import { ReduxRouter }        from 'redux-router';
 import Index                  from './components/layout/index';
