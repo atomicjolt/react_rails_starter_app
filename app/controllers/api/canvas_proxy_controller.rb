44960b34ceb4920af86ee4a1936e8c92017fff70
Justin Ball <justinball@gmail.com>
2016-01-04 17:57:35 -0700

    Pass headers through proxy

--- app/controllers/api/canvas_proxy_controller.rb
+++ app/controllers/api/canvas_proxy_controller.rb
@@ -8,6 +8,16 @@ class Api::CanvasProxyController < ApplicationController
     api = current_account.canvas_api
     result = api.proxy(params[:url], request.method, request.body)
     response.status = result.code
+    
+    allowed_headers = [ "content-type", "link", "p3p", "x-canvas-meta", "x-canvas-user-id", 
+      "x-rate-limit-remaining", "x-request-context-id", "x-request-cost", 
+      "x-request-processor", "x-robots-tag", "x-runtime", "x-session-id", 
+      "x-ua-compatible", "x-xss-protection"]
+
+    result.headers.each do |name, val|
+      response.headers[name] = val if allowed_headers.include?(name)
+    end
+
     render text: result.body
   end
 
