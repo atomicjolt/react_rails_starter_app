c8a90044c208d1936c29d4e44b475b69fed43cd5
Benjamin Call <llacneb@gmail.com>
2016-01-11 15:13:51 -0700

    admin.js

--- client/js/constants/canvas/admin.js
+++ client/js/constants/canvas/admin.js
@@ -7,6 +7,7 @@ const CanvasMethods = {
   // `return canvasRequest(CanvasConstants.ACCOUNT_ADMINS, {accountId}, query);`
   "ACCOUNT_ADMINS": Network.GET,
 
+
 // [Remove account admin](https://canvas.instructure.com/doc/api/all_resources.html#method.admins.destroy)
 // `return canvasRequest(CanvasConstants.REMOVE_ADMINS, {account_id, user_id}, query);`
   "REMOVE_ADMINS": Network.DEL,
