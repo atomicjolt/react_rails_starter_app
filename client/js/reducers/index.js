caeb9cb2b43ffa6bd90883b99f54514723383350
Justin Ball <justinball@gmail.com>
2016-02-19 17:25:59 -0700

    Clean up

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers }              from 'redux';
 import settings                         from './settings'
 import application                      from './application';
 import messages                         from './messages';
-import { routeReducer }                 from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  routing: routeReducer,
   settings,
   application,
   messages,
