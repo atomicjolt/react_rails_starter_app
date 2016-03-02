d00ce82ca0331a725d1b802d4bc6e4be697d11f6
Justin Ball <justinball@gmail.com>
2016-03-01 21:30:26 -0700

    Fixes spec to work with new canvas libs

--- client/js/libs/canvas/action.spec.js
+++ client/js/libs/canvas/action.spec.js
@@ -4,11 +4,11 @@ import _                     from "lodash";
 describe('canvasRequest', () => {
 
   it('Generates an action that can be dispatched to canvas middleware', () => {
-
-    const action = canvasRequest("ASSESSMENTS", {course_id: 1});
+    const endpoint = { type: "LIST_STUDENTS", method: "get", reducer: 'courses'};
+    const action = canvasRequest(endpoint, {course_id: 1});
     expect(action.params.course_id).toBe(1);
     expect(action.canvas).toBe(true);
-    expect(action.type).toBe("ASSESSMENTS");
+    expect(action.type).toBe(endpoint.type);
   
   });
 
