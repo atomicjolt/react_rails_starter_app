e543433d68b68653ba70aac7a63e81091a7d0bd1
Justin Ball <justinball@gmail.com>
2016-04-13 18:52:23 -0600

    Updates to latest node, updates packages, adds production performance optimizations

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -14,11 +14,25 @@ module.exports = function(stage){
   ];
 
   var autoprefix = '{browsers:["Android 2.3", "Android >= 4", "Chrome >= 20", "Firefox >= 24", "Explorer >= 8", "iOS >= 6", "Opera >= 12", "Safari >= 6"]}';
-  
-  var babel = 'babel?plugins[]=transform-runtime,plugins[]=transform-decorators-legacy&plugins[]=transform-class-properties&presets[]=react,presets[]=es2015,presets[]=stage-0';
+
+  var plugins = 'plugins[]=transform-runtime' +             // Externalise references to helpers and builtins, automatically polyfilling your code without polluting globals.
+                ',plugins[]=transform-decorators-legacy' +  // A plugin for Babel 6 that (mostly) replicates the old decorator behavior from Babel 5. Decorators aren't part of the standard yet. This gives us a good enough solution for now.
+                ',plugins[]=transform-class-properties';    // Allows class instance fields and class static properties.
+
+  var presets = 'presets[]=react' +  // Include all plugins needed for React
+                ',presets[]=es2015' + // Include all plugins needed to handle es2015 syntax
+                ',presets[]=stage-0'; // Enables experimental ES features.
+
   if(stage == "development"){
-    babel = babel + ',presets[]=react-hmre';
+    presets = presets + ',presets[]=react-hmre'; // Adds react hot module reload
+  } else if (stage == "production") {
+    plugins = plugins + ',plugins[]=transform-react-constant-elements'; // Hoists static React components to reduce calls to createElement
+    plugins = plugins + ',plugins[]=transform-react-inline-elements';   // Replaces the React.createElement function with a more optimized one for production
+    plugins = plugins + ',transform-react-remove-prop-types';           // Removes prop types from code
   }
+
+  var babel   = 'babel?' + plugins + '&' + presets;
+
   var jsLoaders = [babel];
 
   var cssLoaders = ['css-loader', 'autoprefixer-loader?' + autoprefix];
@@ -28,7 +42,7 @@ module.exports = function(stage){
 
   var lessLoaders = cssLoaders.slice(0);
       lessLoaders.push('less-loader');
- 
+
   var entries = _.cloneDeep(settings.entries);
 
   var cssEntries = settings.cssEntries;
@@ -53,10 +67,10 @@ module.exports = function(stage){
     context: __dirname,
     entry: entries,
     output: {
-      path: release ? settings.prodOutput : settings.devOutput,
+      path: release ? settings.prodOutput : settings.devOutput,                                                // Location where generated files will be output
       filename: release ? '[name]-[chunkhash]' + settings.buildSuffix : '[name]' + settings.buildSuffix,
       chunkFilename: release ? '[id]-[chunkhash]' + settings.buildSuffix : '[id].js',
-      publicPath: release ? settings.prodRelativeOutput : settings.devAssetsUrl + settings.devRelativeOutput,
+      publicPath: release ? settings.prodRelativeOutput : settings.devAssetsUrl + settings.devRelativeOutput,  // Public path indicates where the assets will be served from. In dev this will likely be localhost or a local domain. In production this could be a CDN.
       sourceMapFilename: 'debugging/[file].map',
       pathinfo: !release // http://webpack.github.io/docs/configuration.html#output-pathinfo
     },
