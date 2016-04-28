ac376b8f0f584f04a1888041e9933e7b97e4b9b2
Justin Ball <justinball@gmail.com>
2016-04-27 22:21:27 -0600

    No gulp. Just scripts.

--- client/webpack.hot.js
+++ client/webpack.hot.js
@@ -1,35 +1,44 @@
 var release              = false;
-var express              = require('express');
-var webpack              = require('webpack');
-var webpackMiddleware    = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var settings             = require('./config/settings.js');
-var webpackConfig        = require('./config/webpack.config')("development");
-var path                 = require('path');
+var express              = require("express");
+var webpack              = require("webpack");
+var webpackMiddleware    = require("webpack-dev-middleware");
+var webpackHotMiddleware = require("webpack-hot-middleware");
+var settings             = require("./config/settings.js");
+var webpackConfig        = require("./config/webpack.config")("development");
+var path                 = require("path");
 var app                  = express();
-var compiler             = webpack(webpackConfig);
 
-app.use(express.static(settings.devOutput));
+var build                = require("./tools/build");
 
-app.use(webpackMiddleware(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath,
-  watch: true,
-  noInfo: true,
-  headers: { 'Access-Control-Allow-Origin': '*' }
-}));
+function launch(){
 
-app.use(webpackHotMiddleware(compiler));
+  var compiler                  = webpack(webpackConfig);
+  var webpackMiddlewareInstance = webpackMiddleware(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    watch: true,
+    noInfo: true,
+    headers: { "Access-Control-Allow-Origin": "*" }
+  });
 
-app.get('*', function response(req, res) {
-  res.sendFile(path.join(settings.devOutput, req.url));
-});
+  app.use(express.static(settings.devOutput));
 
-app.listen(settings.hotPort, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Listening on: ' + webpackConfig.output.publicPath);
-  console.log('Serving content from: ' + settings.devOutput);
-});
+  app.use(webpackMiddlewareInstance);
+
+  app.use(webpackHotMiddleware(compiler));
+
+  app.get("*", function response(req, res) {
+    res.sendFile(path.join(settings.devOutput, req.url));
+  });
+
+  app.listen(settings.hotPort, "localhost", function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Listening on: " + webpackConfig.output.publicPath);
+    console.log("Serving content from: " + settings.devOutput);
+  });
+}
+
+build.watch(launch);
