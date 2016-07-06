d4e432f39e430b7910303e9aa204bb5a3bc3bfd9
Justin Ball <justinball@gmail.com>
2016-07-06 16:27:29 -0600

    Removes messages reducer and actions

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -1,14 +1,12 @@
 import { combineReducers }  from 'redux';
 import settings             from './settings';
 import application          from './application';
-import messages             from './messages';
 import jwt                  from './jwt';
 
 const rootReducer = combineReducers({
   settings,
   jwt,
-  application,
-  messages
+  application
 });
 
 export default rootReducer;
