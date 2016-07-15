54404a4923f2b96abae31fb7715562c395d17a87
Justin Ball <justinball@gmail.com>
2016-07-15 15:44:07 -0600

    Adds static direcctory for copying files into build without renamees

--- client/tools/build/index.js
+++ client/tools/build/index.js
@@ -1,6 +1,6 @@
 var path          = require("path");
 var _             = require("lodash");
-var fs            = require("fs");
+var fs            = require("fs-extra");
 var webpack       = require("webpack");
 var nodeWatch     = require("node-watch");
 var del           = require("del");
@@ -107,8 +107,18 @@ function buildContents(inputPath, outputPath, webpackConfig, webpackStats, stage
 // -----------------------------------------------------------------------------
 function build(isHot){
   return new Promise(function(resolve, reject){
-    console.log("Building files in: " + inputPath);
-    del(outputPath, {force: true}).then(function(){ // Delete everything in the output path
+
+    // Delete everything in the output path
+    del(outputPath, {force: true}).then(function(){
+
+      // Copy static files to build directory
+      if(settings.staticDir){
+        console.log("Copying static files in " + settings.staticDir);
+        fs.copySync(settings.staticDir, outputPath);
+      }
+
+      // Build files
+      console.log("Building files in: " + inputPath);
       buildWebpackEntries(isHot).then(function(packResults){
         var pages = buildContents(inputPath, outputPath, packResults.webpackConfig, packResults.webpackStats, stage, options);
 
