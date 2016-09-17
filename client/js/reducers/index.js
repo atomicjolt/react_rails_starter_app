12b8e28761e69ccc82bbda45266b5edcbea4c2b3
Justin Ball <justinball@gmail.com>
2016-09-16 22:51:53 -0600

    Remove Canvas reducer

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -2,13 +2,12 @@ import { combineReducers }  from 'redux';
 import settings             from './settings';
 import application          from './application';
 import jwt                  from './jwt';
-import canvas               from '../libs/canvas/reducer';
+//import canvas               from '../libs/canvas/reducer';
 
 const rootReducer = combineReducers({
   settings,
   jwt,
-  application,
-  canvas
+  application
 });
 
 export default rootReducer;
