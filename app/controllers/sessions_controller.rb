f20a64febd533e230478b77b1e1ade561fb70139
Justin Ball <justinball@gmail.com>
2016-09-19 12:36:48 -0600

    Removes whitespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,8 +19,8 @@ class SessionsController < Devise::SessionsController
     respond_with resource, location: after_sign_in_path_for(resource) do |format|
       format.json { render json: {
         userId: resource.id,
-        email: resource.email, 
-        displayName: resource.name, 
+        email: resource.email,
+        displayName: resource.name,
         jwt_token: token
       } }
     end
