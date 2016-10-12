620544752e69286d3e060e6ecb5f7f1eadd12008
Justin Ball <justinball@gmail.com>
2016-10-12 13:34:52 -0600

    Extract paging into concern

---
+++ spec/controllers/concerns/paging_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe ApplicationController, type: :controller do
+
+  controller do
+    include Paging
+
+    respond_to :json
+
+    def index
+
+    end
+  end
+
+  describe "paging" do
+    it "should add paging to controller" do
+      get :index, format: :json
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
