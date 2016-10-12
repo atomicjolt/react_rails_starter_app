a9606583cc890b84ca463e25ce2a33e2d8cf5ceb
Justin Ball <justinball@gmail.com>
2016-10-12 13:35:57 -0600

    Don't send account to ability class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,6 +56,7 @@ class ApplicationController < ActionController::Base
   private
 
     def current_ability
+      @current_ability ||= Ability.new(current_user)
     end
 
     def user_not_authorized
