b5e8854a73ddd9a23332590b0ed69a438ac03ae9
Justin Ball <justinball@gmail.com>
2016-10-25 17:14:15 -0600

    Fix loading a new jwt

--- client/js/reducers/jwt.js
+++ client/js/reducers/jwt.js
@@ -7,7 +7,7 @@ export default (state = initialState, action) => {
   switch(action.type){
 
     case JwtConstants.REFRESH_JWT:
-      return action.payload;
+      return action.payload.jwt;
       break;
 
     default:
