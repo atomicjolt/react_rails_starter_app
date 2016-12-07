4fef5c6366928d9bf4859f1b622ba6b5ed29f462
bensmith <benhsmith13@gmail.com>
2016-12-07 16:16:02 -0700

    double trouble

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'coveralls'
-Coveralls.wear!('rails')
+require "coveralls"
+Coveralls.wear!("rails")
 
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
