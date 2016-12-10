ed8b08bc5d659d4b403e57756a7bdfe01e550273
Justin Ball <justinball@gmail.com>
2016-12-09 17:37:52 -0700

    Formatting to match style rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,26 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    rescue_from CanCan::AccessDenied do |exception|
-      respond_to do |format|
-        format.html { redirect_to root_url, alert: exception.message }
-        format.json { render json: { error: exception.message }, status: :unauthorized }
-      end
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_url, alert: exception.message }
+      format.json { render json: { error: exception.message }, status: :unauthorized }
     end
+  end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   private
 
-    def current_ability
-      @current_ability ||= Ability.new(current_user)
-    end
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
 
-    def user_not_authorized
-      render :file => "public/401.html", :status => :unauthorized
-    end
+  def user_not_authorized
+    render :file => "public/401.html", :status => :unauthorized
+  end
 
 end
