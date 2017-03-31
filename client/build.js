78dd9311fc2efa187162d908d7fe291ed3dddf23
Justin Ball <justinball@gmail.com>
2017-03-30 22:00:58 -0600

    Removes test

--- client/build.js
+++ client/build.js
@@ -2,7 +2,6 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const apps = require('./libs/build/apps');
 
-//const stage = argv.release ? 'production' : 'development';
+const stage = argv.release ? 'production' : 'development';
 
-const stage = 'production';
 apps.buildApps(stage, argv.onlyPack);
