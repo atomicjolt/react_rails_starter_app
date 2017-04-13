0bc7c7924f528c62074ca101be81dc33d0b8ce50
Justin Ball <justinball@gmail.com>
2017-04-13 17:19:59 -0600

    Copied default rails 5 stuff

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@
 
 source "https://rubygems.org"
 
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails"
 gem "rails", "5.0.2"
 
@@ -87,3 +92,6 @@ end
 group :production do
   gem "rails_12factor"
 end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_lti_application
-
   protected
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -20,10 +18,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
-    def current_lti_application
-      @current_lti_application ||= LtiApplication.find_by(lti_key: params[:oauth_consumer_key])
-    end
 
   def current_ability
     @current_ability ||= Ability.new(current_user)
