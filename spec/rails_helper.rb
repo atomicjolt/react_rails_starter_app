ea780b047f2e9d6d97cf1c7431415c7a7ede52cf
Justin Ball <justinball@gmail.com>
2017-04-13 16:02:33 -0600

    Match master rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false # Spec in canvas_spec.rb tests pessimistic locking so we have to set transactional fixtures false
+  config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -52,7 +52,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     begin
-      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.strategy = :transaction
       DatabaseCleaner.clean_with(:truncation)
       DatabaseCleaner.start
       # FactoryGirl.lint
