e5f681f09af8dff8aaa15772a05b3e532ef72b62
Justin Ball <justinball@gmail.com>
2017-04-29 15:28:43 -0600

    Reduce output noise

--- client/libs/build/content.js
+++ client/libs/build/content.js
@@ -58,9 +58,10 @@ function buildContent(fullPath, app, webpackAssets, ext) {
     }
   } catch (err) {
     log.error(`Unable to compile html from ${fullPath}`);
-    log.error(err);
-    log.error('Call stack');
-    log.error(err.stack);
+    // Uncomment the following for more details
+    // log.error(err);
+    // log.error('Call stack');
+    // log.error(err.stack);
   }
 
   // Apply template
