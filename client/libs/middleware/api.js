097aa391ec6fb7195768f87c199e47b785e35ca5
Justin Ball <justinball@gmail.com>
2017-05-01 18:04:49 -0600

    Export the apiRequest from middleware.

--- client/libs/middleware/api.js
+++ client/libs/middleware/api.js
@@ -1,44 +1,47 @@
 import api      from '../api/api';
 import { DONE } from '../constants/wrapper';
 
-const API = store => next => (action) => {
-  function request(method, url, params, body, headers, timeout) {
-    const state = store.getState();
-    const promise = api.execRequest(
-      method,
-      url,
-      state.settings.apiUrl,
-      state.jwt,
-      state.settings.csrfToken,
-      params,
-      body,
-      headers,
-      timeout);
+export function apiRequest(store, action) {
+  const state = store.getState();
+  const promise = api.execRequest(
+    action.method,
+    action.url,
+    state.settings.apiUrl,
+    state.jwt,
+    state.settings.csrfToken,
+    action.params,
+    action.body,
+    action.headers,
+    action.timeout);
 
-    if (promise) {
-      promise.then(
-        (response) => {
-          store.dispatch({
-            type     : action.type + DONE,
-            payload  : response.body,
-            original : action,
-            response,
-          }); // Dispatch the new data
-        },
-        (error) => {
-          store.dispatch({
-            type     : action.type + DONE,
-            payload  : {},
-            original : action,
-            error,
-          }); // Dispatch the new error
-        },
-      );
-    }
+  if (promise) {
+    promise.then(
+      (response) => {
+        store.dispatch({
+          type     : action.type + DONE,
+          payload  : response.body,
+          original : action,
+          response,
+        }); // Dispatch the new data
+      },
+      (error) => {
+        store.dispatch({
+          type     : action.type + DONE,
+          payload  : {},
+          original : action,
+          error,
+        }); // Dispatch the new error
+      },
+    );
   }
 
+  return promise;
+}
+
+const API = store => next => (action) => {
+
   if (action.method) {
-    request(action.method, action.url, action.params, action.body, action.headers, action.timeout);
+    apiRequest(store, action);
   }
 
   // call the next middleWare
