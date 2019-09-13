8cc89f8867eb16f23412665c667f2ca81703b7bd
Justin Ball <justinball@gmail.com>
2019-09-12 20:58:36 -0600

    import the runtime instead

--- client/apps/hello_world/app.jsx
+++ client/apps/hello_world/app.jsx
@@ -1,5 +1,5 @@
 import 'core-js';
-import 'regenerator-runtime';
+import 'regenerator-runtime/runtime';
 import es6Promise from 'es6-promise';
 import React from 'react';
 import ReactDOM from 'react-dom';
