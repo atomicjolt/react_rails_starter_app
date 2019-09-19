4406d986ad1e88c76f5bf19fc122df3df0ab8a22
Justin Ball <justinball@gmail.com>
2019-09-18 20:56:43 -0600

    Adds support for .gql and .graphql files

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker');
 
+const graphqlLoader = {
+  test: /\.(graphql|gql)$/,
+  exclude: /node_modules/,
+  loader: 'graphql-tag/loader'
+};
+
+// Insert json loader at the end of list
+environment.loaders.append('graphql', graphqlLoader);
+
 module.exports = environment;
