7c30a233dd2aaa85f08a2307239140bf1c4ef58d
James Carbine <jamescarbine@gmail.com>
2020-03-13 12:58:46 -0600

    Lower to 64
    A limit of 72 can give away that we are using bcrypt.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -137,7 +137,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 14..72
+  config.password_length = 14..64
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
